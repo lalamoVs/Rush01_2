d824b136271bf5d0fd2dae59f511278a88849413
lalamoVs <jasonwong22015@gmail.com>
2026-04-19 14:56:56 +0800

    Upload

--- ex00/columns.c
+++ ex00/columns.c
@@ -13,26 +13,64 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void	valid(int *sample);
+int	count_visible(int line[4]);
 
-void	column(int *top, int *bottom)
+int	check_visible_col(int *board, int column[2][4], int c)
 {
-	valid(top);
-    valid(bottom);
+	int	r;
+	int	line[4];
 
-    // int	column[1][4];
-    // int	i;
-    // int	clues;
-
-    // i = 0;
-    // clues = 0;
-    // while (i < 4)
-    // {
-	// 	if ((top[i] == 4 && bottom[i] != 1) || (top[i] == 3 && bottom[i] != 2))
-    //     {
-    //         write(1, "Error\n", 1);
-    //     }
-    //     i ++;
-    // }
-
+	r = 0;
+	while (r < 4)
+	{
+		line[r] = board[r * 4 + c];
+		r ++;
+	}
+	if (count_visible(line) != column[0][c])
+		return (0);
+	r = 0;
+	while (r < 4)
+	{
+		line[r] = board[(3 - r) * 4 + c];
+		r ++;
+	}
+	if (count_visible(line) != column[1][c])
+		return (0);
+	return (1);
 }
+
+int	check_cols(int *board, int column[2][4])
+{
+	int	r;
+	int	complete;
+	int	c;
+
+	c = 0;
+	while (c < 4)
+	{
+		complete = 1;
+		r = 0;
+		while (r < 4)
+		{
+			if (board[r * 4 + c] == 0)
+				complete = 0;
+			r++;
+		}
+		if (complete)
+		{
+			if (!check_visible_col(board, column, c))
+				return (0);
+		}
+		c++;
+	}
+	return (1);
+}
+
+// void	valid(int *sample);
+
+// void	column(int *top, int *bottom)
+// {
+// 	valid(top);
+//     valid(bottom);
+
+// }
--- ex00/rows.c
+++ ex00/rows.c
@@ -13,10 +13,65 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void	valid(int *sample);
+int	check_cols(int *board, int column[2][4]);
 
-void	row(int *left, int *right)
+int	count_visible(int line[4]);
+
+int	check_visible_row(int *board, int row[2][4], int r)
 {
-    valid(left);
-    valid(right);
+	int	line[4];
+	int	c;
+
+	c = 0;
+	while (c < 4)
+	{
+		line[c] = board[r * 4 + c];
+		c++;
+	}
+	if (count_visible(line) != row[0][r])
+		return (0);
+	c = 0;
+	while (c < 4)
+	{
+		line[c] = board[r * 4 + (3 - c)];
+		c++;
+	}
+	if (count_visible(line) != row[1][r])
+		return (0);
+	return (1);
 }
+
+int	check_rows(int *board, int column[2][4], int rows[2][4])
+{
+	int	r;
+	int	c;
+	int	complete;
+
+	r = 0;
+	while (r < 4)
+	{
+		complete = 1;
+		c = 0;
+		while (c < 4)
+		{
+			if (board[r * 4 + c] == 0)
+				complete = 0;
+			c++;
+		}
+		if (complete)
+		{
+			if (!check_visible_row(board, rows, r))
+				return (0);
+		}
+		r++;
+	}
+	return (check_cols(board, column));
+}
+
+// void	valid(int *sample);
+
+// void	row(int *left, int *right)
+// {
+//     valid(left);
+//     valid(right);
+// }
